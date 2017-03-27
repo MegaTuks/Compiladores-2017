671941bfc2f018e7c4a366a39ab4712bd4435fdc
MegaTuks <a_marcel93@hotmail.com>
2017-03-26 23:31:31 -0600

    add correct cuadruples

--- tablas.py
+++ tablas.py
@@ -1,4 +1,4 @@
-th# cubo semantico es un diccionario de matrices que tiene de Id los tipos de operador que puede haber
+# cubo semantico es un diccionario de matrices que tiene de Id los tipos de operador que puede haber
 # Ejemplo de como son cada una
 # bool=0,int=1,float =2 ,string = 3,error = 4
 # +, [
@@ -67,3 +67,45 @@ class claseCuboSemantico:
         else:
             print("\nERROR. Tipos de datos:", operando1, ",", operando2, "y/o operador:", operador, "desconocidos.")
             return None
+
+class Cuadruplos:
+    def __init__(self):
+        self.cuadruplos = list()
+
+    def normalCuad(self, operador=None, operando1=None, operando2=None, destino=None):
+        self.cuadruplos.append((operador, operando1, operando2, destino))
+        print("operador:" ,operador , " op1:",operando1, " op2:", operando2 , " destino:",destino)
+
+    def updateCuad(self, index, operador=None, operando1=None, operando2=None, destino=None):
+        self.cuadruplos[index] = (operador, operando1, operando2, destino)
+
+    def AssignCuad(self,operador, operando1, destino):
+        self.cuadruplos.append((operador, operando1, None, destino))
+
+    def SaltaCuad(self, Goto, destino=None):
+      self.cuadruplos.append((Goto, None, "1", destino))
+      return len(self.cuadruplos) - 1
+      print("ver como codigicar saltos")
+
+    def AgregarSalto(self, indice, expr, destino=None):
+      if destino is None:
+        destino = len(self.cuadruplos)
+      salto = (self.cuadruplos[indice][0], expr, "2", destino)
+      self.cuadruplos[indice] = salto
+      print("darle update al cuadruplo")
+
+    def EspecialCuad(self, operador, operando1, operando2, destino):
+        print("cuadruplo a usar en funciones especiales")
+
+    def CuadSize(self):
+        return len(self.cuadruplos)
+
+    def Ultimo(self):
+        return self.cuadruplos[-1]
+
+    def imprimir(self):
+        indice = 0
+        for cuad in self.cuadruplos:
+            print('indice:', indice, 'operador: ', cuad[0], 'operando1: ', cuad[1], 'operando2: ', cuad[2], 'destino:',
+                  cuad[3])
+            indice = indice + 1
